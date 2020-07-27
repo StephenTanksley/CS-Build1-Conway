036f2f76581e242c3825619a8367bd68c8ef53e2
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-07-27 14:40:59 -0500

    Updated styles.

--- conway/src/styles/styled.js
+++ conway/src/styles/styled.js
@@ -7,3 +7,18 @@ export const Container = styled.div`
   justify-content: center;
   align-items: center;
 `;
+
+export const GridContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  display: "grid";
+  grid-template-columns: ${(props) => `repeat(${props.size}, "18px")`};
+`;
+
+export const ButtonContainer = styled(Container)`
+  flex-direction: row;
+  padding: 1rem;
+`;
