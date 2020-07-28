e4a83ee6807e751b1cf41ff5ea5e7a9f93ef77be
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-07-27 20:11:28 -0500

    feat: Logic updated.
    I'm working through the logic to run updates based on the values of the neighboring cells.

--- conway/src/components/Board.js
+++ conway/src/components/Board.js
@@ -18,53 +18,59 @@ const Board = () => {
     return grid;
   });
 
-  const specific_key = localGrid.filter(function (cell, i) {
-    return cell["row"] === 4 && cell["col"] === 3;
-  });
+  console.log("Context grid: ", grid);
+  console.log("Local grid: ", localGrid);
 
-  console.log("specific key: ", specific_key);
+  // const specific_key = localGrid.filter(function (cell, i) {
+  //   return cell["row"] === 4 && cell["col"] === 3;
+  // });
+  // console.log("specific key alive: ", specific_key[0].alive);
 
-  const positionInGrid = [localGrid[15]["row"], localGrid[15]["col"]];
-  console.log(positionInGrid);
+  // const positionInGrid = [localGrid[15]["row"], localGrid[15]["col"]];
+  // console.log(positionInGrid);
 
   const width = Math.round((window.innerWidth * 0.35) / size);
-  console.log(width);
 
-  console.log("local grid: ", localGrid[15]);
-
-  // const updateGrid = useCallback((grid) => {
-  //   if (!runningRef.current) {
-  //         return;
+  // const findItems = () => {
+  //   for (let i = 0; i < localGrid.length; i++) {
+  //     const item = localGrid[i];
+  //     console.log("alive or dead: ", item.alive);
   //   }
-  //   setLocalGrid((localGrid) => {
-  //     return produce(localGrid, (gridCopy) => {
-  //       for (let i = 0; i < localGrid.length; i++){
-  //         for (let j = 0; j < localGrid.length; j++){
-  //           We look here into the current value of
-  //           const current_row = localGrid[i]['row']
-  //           const current_col = localGrid[i]['col']
+  // };
 
-  //           let neighborsCount = 0;
-  //           localGrid.forEach(([x, y]) => {
-  //             const current_i = current_row + x;
-  //             const current_j = current_col + y;
+  // findItems();
 
-  //           })
-  //         }
-  //       }
-  //     })
-  //   })
-  // });
+  const updateGrid = useCallback((grid) => {
+    if (!runningRef.current) {
+      return;
+    }
+    setLocalGrid((localGrid) => {
+      return produce(localGrid, (gridCopy) => {
+        for (let i = 0; i < localGrid.length; i++) {
+          console.log(localGrid[i]);
+        }
+      });
+    });
+  });
+
+  // useEffect(() => {
+  //   if (size) {
+  //     const rando = randomCellBoard(size);
+  //     console.log(`random cell board of size ${size} : `, rando);
+  //   }
+  // }, [size]);
 
   useEffect(() => {
-    if (size) {
-      const rando = randomCellBoard(size);
-      console.log(`random cell board of size ${size} : `, rando);
-    }
-  }, [size]);
+    setLocalGrid(grid);
+    console.log(grid);
+  }, [grid]);
 
   const { dispatch } = state;
   const { RUNNING, STOP_RUNNING, NEXT_GEN, RANDOM_BOARD, CLEAR } = ACTIONS;
+
+  // const dispatchAction = (action) => {
+  //   dispatch({ type: action });
+  // };
 
   const generationsRef = useRef(generations);
   generationsRef.current = generations;
@@ -72,38 +78,12 @@ const Board = () => {
   const runningRef = useRef(running);
   runningRef.current = running;
 
-  // const runSimulation = useCallback(() => {
-  //   dispatch({ type: NEXT_GEN });
-  //   if (!runningRef.current) {
-  //     return;
-  //   }
+  const handleClick = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
 
-  //   setLocalGrid((g) => {
-  //     return produce(g, (gridCopy) => {
-  //       console.log("value of g: ", g);
-  //       for (let i = 0; i < size; i++) {
-  //         for (let k = 0; k < size; k++) {
-  //           let neighborCount = 0;
-  //           neighbors.forEach(([x, y]) => {
-  //             const newI = i + x;
-  //             const newK = k + y;
-  //             if (newI >= 0 && newI < size && newK >= 0 && newK < size) {
-  //               neighborCount += g[newI][newK];
-  //             }
-  //           });
-
-  //           if (neighborCount < 2 || neighborCount > 3) {
-  //             gridCopy[i][k] = 0;
-  //           } else if (g[i][k] === 0 && neighborCount === 3) {
-  //             gridCopy[i][k] = 1;
-  //           }
-  //         }
-  //       }
-  //     });
-  //   });
-
-  //   setTimeout(runSimulation, speed);
-  // }, []);
+    console.log(e.target);
+  };
 
   return (
     <>
@@ -129,6 +109,7 @@ const Board = () => {
                 className={cell.alive ? "alive" : "dead"}
                 alive={cell.alive}
                 active={cell.active}
+                onClick={handleClick}
               />
             );
           })}
@@ -144,7 +125,13 @@ const Board = () => {
         >
           Clear
         </button>
-        <button>Random</button>
+        <button
+          onClick={() => {
+            dispatch({ type: RANDOM_BOARD });
+          }}
+        >
+          Random
+        </button>
       </ButtonContainer>
     </>
   );
